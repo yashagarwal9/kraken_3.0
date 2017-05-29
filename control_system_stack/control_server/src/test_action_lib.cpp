d007d6bcb0529be98df1171a59146b1b0c0280ef
Yashagarwal9 <agarwal.yash94@outlook.com>
2017-05-29 19:57:14 +0530

    Tested on simulator
    Signed-off-by: Yashagarwal9 <agarwal.yash94@outlook.com>

--- control_system_stack/control_server/src/test_action_lib.cpp
+++ control_system_stack/control_server/src/test_action_lib.cpp
@@ -11,8 +11,8 @@ int main(int argc,char** argv)
 
     // create the action client
     // true causes the client to spin its own thread
-    actionlib::SimpleActionClient<kraken_msgs::advancedControllerAction> ac("/kraken/advance_controller_action", true);
-    actionlib::SimpleActionClient<kraken_msgs::controllerAction> ac1("/kraken/controller_action", true);
+    actionlib::SimpleActionClient<kraken_msgs::advancedControllerAction> ac("/kraken/control/advancedcontrol_action", true);
+    actionlib::SimpleActionClient<kraken_msgs::controllerAction> ac1("/kraken/control/setpoint_action", true);
     ROS_INFO("Waiting for action server to start.");
     // wait for the action server to start
     ac.waitForServer(); //will wait for infinite time
@@ -22,20 +22,19 @@ int main(int argc,char** argv)
     // send a goal to the action
     kraken_msgs::advancedControllerGoal goal;
     kraken_msgs::controllerGoal goal1;
-
-    if(atoi(argv[1])==1)
+	
+    if(atoi(argv[1])==0)
     {
-        goal.flag = atof(argv[2]);
-        goal.x = atof(argv[3]);
-        goal.y = atof(argv[4]);
-        goal.depth = atof(argv[5]);
-
+        goal.flag = atof(argv[1]);
+        goal.x = atof(argv[2]);
+        goal.y = atof(argv[3]);
+        goal.depth = atof(argv[4]);
         ac.sendGoal(goal);
         //ac.sendGoal(goal);
         ROS_INFO("Goal sent");
         std::cerr <<"x:"<<goal.x<<"\ty:"<<goal.y<<"\tz:"<<goal.depth<<std::endl;
         //wait for the action to return
-        bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+        bool finished_before_timeout = ac.waitForResult();//ros::Duration(30.0));
 
         if (finished_before_timeout)
         {
@@ -50,9 +49,10 @@ int main(int argc,char** argv)
     }
     else
     {
-        goal1.p = atof(argv[3]);
-        goal1.r = atof(argv[4]);
-        goal1.y = atof(argv[5]);
+    		goal.flag = 0;
+        goal1.p = atof(argv[2]);
+        goal1.r = atof(argv[3]);
+        goal1.y = atof(argv[4]);
         goal1.d = 0.0;
         ac1.sendGoal(goal1);
         //ac.sendGoal(goal);

fb102d522ad49ff7dfb67b96e1407cdb5b8697c1
Yashagarwal9 <agarwal.yash94@outlook.com>
2017-05-29 19:53:29 +0530

    Added Callback function for dynamic configuration
    Signed-off-by: Yashagarwal9 <agarwal.yash94@outlook.com>

--- control_system_stack/control_server/src/AuvController.cpp
+++ control_system_stack/control_server/src/AuvController.cpp
@@ -20,6 +20,8 @@ void AuvController::pause()
 {
     std::cerr<<"Pause"<<std::endl;
     ControlParameters *param = _control_parameters[_control_parameters_index["pause"]];
+    AuvController::curr_file = "Pause";
+    std::cout<<curr_file;
     resetController(param->getGain(), param->getOffset());
 }
 
@@ -27,6 +29,7 @@ void AuvController::moveTest()
 {
     std::cerr<<"Test Movement"<<std::endl;
     ControlParameters *param = _control_parameters[_control_parameters_index["test"]];
+    curr_file = "Test";
     resetController(param->getGain(), param->getOffset());
 }
 
@@ -34,6 +37,7 @@ void AuvController::moveBack()
 {
     std::cerr<<"Moving back"<<std::endl;
     ControlParameters *param = _control_parameters[_control_parameters_index["moveback"]];
+    curr_file = "MoveBack";
     resetController(param->getGain(), param->getOffset());
 }
 
@@ -41,6 +45,7 @@ void AuvController::moveForward()
 {
     std::cerr<<"Moving forward"<<std::endl;
     ControlParameters *param = _control_parameters[_control_parameters_index["moveforward"]];
+    curr_file = "MoveForward";
     resetController(param->getGain(), param->getOffset());
 }
 
@@ -115,6 +120,7 @@ void AuvController::doControlIteration(const kraken_msgs::krakenPose feedback)
         {
 
             _thruster_data6.data[row] +=  _gain[row][col] * _error[col];
+            //ROS_INFO("%lf %d ", _error[col] ,col);
 
         }
     }
@@ -129,9 +135,9 @@ void AuvController::doControlIteration(const kraken_msgs::krakenPose feedback)
 void AuvController::updateIPState()
 {
 
-    _error[0] = _setPoint[0];// - _feedback.data[0];
-    _error[1] = _setPoint[1];// - _feedback.data[1];
-    _error[2] = _setPoint[2];// - _feedback.data[2];
+    _error[kraken_core::_px] += _feedback.data[kraken_core::_px];
+    _error[kraken_core::_py] += _feedback.data[kraken_core::_py];
+    _error[kraken_core::_pz] += _feedback.data[kraken_core::_pz];
 
     _error[kraken_core::_px+3] = _feedback.data[kraken_core::_px];
     _error[kraken_core::_py+3] = _feedback.data[kraken_core::_py];
@@ -145,9 +151,9 @@ void AuvController::updateIPState()
     _error[kraken_core::_ay+3] = _feedback.data[kraken_core::_ay];
     _error[kraken_core::_az+3] = _feedback.data[kraken_core::_az];
 
-    _error[kraken_core::_roll+3] = _setPoint[kraken_core::_roll+3];// - _feedback.data[kraken_core::_roll];
-    _error[kraken_core::_pitch+3] = _setPoint[kraken_core::_pitch+3];// - _feedback.data[kraken_core::_pitch];
-    _error[kraken_core::_yaw+3] = _setPoint[kraken_core::_yaw+3];// - _feedback.data[kraken_core::_yaw];
+    _error[kraken_core::_roll+3] += _setPoint[kraken_core::_roll+3];// - _feedback.data[kraken_core::_roll];
+    _error[kraken_core::_pitch+3] += _setPoint[kraken_core::_pitch+3];// - _feedback.data[kraken_core::_pitch];
+    _error[kraken_core::_yaw+3] += _setPoint[kraken_core::_yaw+3];// - _feedback.data[kraken_core::_yaw];
 
     _error[kraken_core::_roll+6] =  _feedback.data[kraken_core::_roll];
     _error[kraken_core::_pitch+6] = _feedback.data[kraken_core::_pitch];
@@ -188,9 +194,9 @@ void AuvController::updateState()
 
 
     float _error1[21];
-    _error1[0] = _setPoint[0];// - _feedback.data[0];
-    _error1[1] = _setPoint[1];// - _feedback.data[1];
-    _error1[2] = _setPoint[2];// - _feedback.data[2];
+    /*_error1[0] = 0;//_setPoint[0];// - _feedback.data[0];
+    _error1[1] = 0;//_setPoint[1];// - _feedback.data[1];
+    _error1[2] = 0;//_setPoint[2];// - _feedback.data[2];
 
     _error1[kraken_core::_px+3] = _setPoint[kraken_core::_px+3] - _feedback.data[kraken_core::_px];
     _error1[kraken_core::_py+3] = _setPoint[kraken_core::_py+3] - _feedback.data[kraken_core::_py];
@@ -200,18 +206,33 @@ void AuvController::updateState()
     _error1[kraken_core::_vy+3] = _setPoint[kraken_core::_vy+3] - _feedback.data[kraken_core::_vy];
     _error1[kraken_core::_vz+3] = _setPoint[kraken_core::_vz+3] - _feedback.data[kraken_core::_vz];
 
+    _error1[kraken_core::_ax+3] = 0;//_setPoint[kraken_core::_ax+3] - _feedback.data[kraken_core::_ax];
+    _error1[kraken_core::_ay+3] = 0;//_setPoint[kraken_core::_ay+3] - _feedback.data[kraken_core::_ay];
+    _error1[kraken_core::_az+3] = 0;//_setPoint[kraken_core::_az+3] - _feedback.data[kraken_core::_az];*/
+    _error[0] += _setPoint[0] - _feedback.data[0];
+    _error[1] += _setPoint[1] - _feedback.data[1];
+    _error[2] += _setPoint[2] - _feedback.data[2];
+
+    _error[kraken_core::_px+3] = _setPoint[kraken_core::_px+3] - _feedback.data[kraken_core::_px];
+    _error[kraken_core::_py+3] = _setPoint[kraken_core::_py+3] - _feedback.data[kraken_core::_py];
+    _error[kraken_core::_pz+3] = _setPoint[kraken_core::_pz+3] - _feedback.data[kraken_core::_pz];
+
+    _error1[kraken_core::_vx+3] = _setPoint[kraken_core::_vx+3] - _feedback.data[kraken_core::_vx];
+    _error1[kraken_core::_vy+3] = _setPoint[kraken_core::_vy+3] - _feedback.data[kraken_core::_vy];
+    _error1[kraken_core::_vz+3] = _setPoint[kraken_core::_vz+3] - _feedback.data[kraken_core::_vz];
+
     _error1[kraken_core::_ax+3] = _setPoint[kraken_core::_ax+3] - _feedback.data[kraken_core::_ax];
     _error1[kraken_core::_ay+3] = _setPoint[kraken_core::_ay+3] - _feedback.data[kraken_core::_ay];
     _error1[kraken_core::_az+3] = _setPoint[kraken_core::_az+3] - _feedback.data[kraken_core::_az];
 
-    multiply(_body_to_world_matrix,&_error1[kraken_core::_px+3],&_error[kraken_core::_px+3]);
-    multiply(_body_to_world_matrix,&_error1[kraken_core::_vx+3],&_error[kraken_core::_vx+3]);
-    multiply(_body_to_world_matrix,&_error1[kraken_core::_ax+3],&_error[kraken_core::_ax+3]);
+    /*multiply(_body_to_world_matrix,&_error1[kraken_core::_px+3],&_error[kraken_core::_px+3]);*/
+    multiply(_body_to_world_matrix, &_error1[kraken_core::_vx+3], &_error[kraken_core::_vx+3]);
+    multiply(_body_to_world_matrix, &_error1[kraken_core::_ax+3], &_error[kraken_core::_ax+3]);
 
 
-    _error[kraken_core::_roll+3] = _setPoint[kraken_core::_roll+3];// - _feedback.data[kraken_core::_roll];
-    _error[kraken_core::_pitch+3] = _setPoint[kraken_core::_pitch+3];// - _feedback.data[kraken_core::_pitch];
-    _error[kraken_core::_yaw+3] = _setPoint[kraken_core::_yaw+3];// - _feedback.data[kraken_core::_yaw];
+    _error[kraken_core::_roll+3] += _setPoint[kraken_core::_roll+3] - _feedback.data[kraken_core::_roll];
+    _error[kraken_core::_pitch+3] += _setPoint[kraken_core::_pitch+3] - _feedback.data[kraken_core::_pitch];
+    _error[kraken_core::_yaw+3] += _setPoint[kraken_core::_yaw+3] - _feedback.data[kraken_core::_yaw];
 
     _error[kraken_core::_roll+6] = _setPoint[kraken_core::_roll+6] - _feedback.data[kraken_core::_roll];
     _error[kraken_core::_pitch+6] = _setPoint[kraken_core::_pitch+6] - _feedback.data[kraken_core::_pitch];
@@ -345,17 +366,17 @@ void AuvController::local2globalAll(kraken_msgs::krakenPose &local, kraken_msgs:
     multiply(_body_to_world_matrix,&_local[kraken_core::_vx+3],&_error[kraken_core::_vx+3]);
     multiply(_body_to_world_matrix,&_local[kraken_core::_ax+3],&_error[kraken_core::_ax+3]);
 
-    global.data[kraken_core::_px] += _global[kraken_core::_px];
-    global.data[kraken_core::_py] += _global[kraken_core::_py];
-    global.data[kraken_core::_pz] += _global[kraken_core::_pz];
+    global.data[kraken_core::_px] = _global[kraken_core::_px];/*+= _global[kraken_core::_px];*/
+    global.data[kraken_core::_py] = _global[kraken_core::_py];/*+= _global[kraken_core::_py];*/
+    global.data[kraken_core::_pz] = _global[kraken_core::_pz];/*+= _global[kraken_core::_pz];*/
 
-    global.data[kraken_core::_vx] += _global[kraken_core::_vx];
-    global.data[kraken_core::_vy] += _global[kraken_core::_vy];
-    global.data[kraken_core::_vz] += _global[kraken_core::_vz];
+    global.data[kraken_core::_vx] = _global[kraken_core::_vx];/*+= _global[kraken_core::_vx];*/
+    global.data[kraken_core::_vy] = _global[kraken_core::_vy];/*+= _global[kraken_core::_vy];*/
+    global.data[kraken_core::_vz] = _global[kraken_core::_vz];/*+= _global[kraken_core::_vz];*/
 
-    global.data[kraken_core::_ax] += _global[kraken_core::_ax];
-    global.data[kraken_core::_ay] += _global[kraken_core::_ay];
-    global.data[kraken_core::_az] += _global[kraken_core::_az];
+    global.data[kraken_core::_ax] = _global[kraken_core::_ax];/*+= _global[kraken_core::_ax];*/
+    global.data[kraken_core::_ay] = _global[kraken_core::_ay];/*+= _global[kraken_core::_ay];*/
+    global.data[kraken_core::_az] = _global[kraken_core::_az];/*+= _global[kraken_core::_az];*/
 
 
     std::cout<<"global x "<<_global[kraken_core::_px]<<" : "<<global.data[kraken_core::_px]<<std::endl;
@@ -419,9 +440,9 @@ void AuvController::local2global(kraken_msgs::krakenPose &local, kraken_msgs::kr
     //multiply(_body_to_world_matrix,&_local[kraken_core::_vx+3],&_error[kraken_core::_vx+3]);
     //multiply(_body_to_world_matrix,&_local[kraken_core::_ax+3],&_error[kraken_core::_ax+3]);
 
-    global.data[kraken_core::_px] += _global[kraken_core::_px];
-    global.data[kraken_core::_py] += _global[kraken_core::_py];
-    global.data[kraken_core::_pz] += _global[kraken_core::_pz];
+    global.data[kraken_core::_px] = _global[kraken_core::_px];/*+= _global[kraken_core::_px];*/
+    global.data[kraken_core::_py] = _global[kraken_core::_py];/*+= _global[kraken_core::_py];*/
+    global.data[kraken_core::_pz] = _global[kraken_core::_pz];/*+= _global[kraken_core::_pz];*/
     /*
      global.data[kraken_core::_vx] += _global[kraken_core::_vx];
      global.data[kraken_core::_vy] += _global[kraken_core::_vy];
@@ -464,7 +485,161 @@ void AuvController::multiply(float matrix[][3], float* src_vec, float* dst_vec)
         dst_vec[i] = val;
     }
 }
-
-
+void AuvController::changeParams(control_server::paramsConfig &msg, int Thruster_selection){
+  int n_map = _control_parameters_index[AuvController::curr_file];
+    float *offset = _control_parameters[n_map]->getOffset();
+    float **gain = _control_parameters[n_map]->getGain();
+        if(Thruster_selection == 0){
+          offset[0] = msg.offset;
+          gain[0][0] = msg.Gain_i_px;
+          gain[0][1] = msg.Gain_i_py;
+          gain[0][2] = msg.Gain_i_pz;
+          gain[0][3] = msg.Gain_px;
+          gain[0][4] = msg.Gain_py;
+          gain[0][5] = msg.Gain_pz;
+          gain[0][6] = msg.Gain_vx;
+          gain[0][7] = msg.Gain_vy;
+          gain[0][8] = msg.Gain_vz;
+          gain[0][9] = msg.Gain_ax;
+          gain[0][10] = msg.Gain_ay;
+          gain[0][11] = msg.Gain_az;
+          gain[0][12] = msg.Gain_i_roll;
+          gain[0][13] = msg.Gain_i_pitch;
+          gain[0][14] = msg.Gain_i_yaw;
+          gain[0][15] = msg.Gain_roll;
+          gain[0][16] = msg.Gain_pitch;
+          gain[0][17] = msg.Gain_yaw;
+          gain[0][18] = msg.Gain_a_roll;
+          gain[0][19] = msg.Gain_a_pitch;
+          gain[0][20] = msg.Gain_a_yaw;
+        }
+        if(Thruster_selection == 1){
+          offset[1] = msg.offset;
+          gain[1][0] = msg.Gain_i_px;
+          gain[1][1] = msg.Gain_i_py;
+          gain[1][2] = msg.Gain_i_pz;
+          gain[1][3] = msg.Gain_px;
+          gain[1][4] = msg.Gain_py;
+          gain[1][5] = msg.Gain_pz;
+          gain[1][6] = msg.Gain_vx;
+          gain[1][7] = msg.Gain_vy;
+          gain[1][8] = msg.Gain_vz;
+          gain[1][9] = msg.Gain_ax;
+          gain[1][10] = msg.Gain_ay;
+          gain[1][11] = msg.Gain_az;
+          gain[1][12] = msg.Gain_i_roll;
+          gain[1][13] = msg.Gain_i_pitch;
+          gain[1][14] = msg.Gain_i_yaw;
+          gain[1][15] = msg.Gain_roll;
+          gain[1][16] = msg.Gain_pitch;
+          gain[1][17] = msg.Gain_yaw;
+          gain[1][18] = msg.Gain_a_roll;
+          gain[1][19] = msg.Gain_a_pitch;
+          gain[1][20] = msg.Gain_a_yaw;
+        }
+        if(Thruster_selection == 2){
+          offset[2] = msg.offset;
+          gain[2][0] = msg.Gain_i_px;
+          gain[2][1] = msg.Gain_i_py;
+          gain[2][2] = msg.Gain_i_pz;
+          gain[2][3] = msg.Gain_px;
+          gain[2][4] = msg.Gain_py;
+          gain[2][5] = msg.Gain_pz;
+          gain[2][6] = msg.Gain_vx;
+          gain[2][7] = msg.Gain_vy;
+          gain[2][8] = msg.Gain_vz;
+          gain[2][9] = msg.Gain_ax;
+          gain[2][10] = msg.Gain_ay;
+          gain[2][11] = msg.Gain_az;
+          gain[2][12] = msg.Gain_i_roll;
+          gain[2][13] = msg.Gain_i_pitch;
+          gain[2][14] = msg.Gain_i_yaw;
+          gain[2][15] = msg.Gain_roll;
+          gain[2][16] = msg.Gain_pitch;
+          gain[2][17] = msg.Gain_yaw;
+          gain[2][18] = msg.Gain_a_roll;
+          gain[2][19] = msg.Gain_a_pitch;
+          gain[2][20] = msg.Gain_a_yaw;
+        }
+        if(Thruster_selection == 3){
+          offset[3] = msg.offset;
+          gain[3][0] = msg.Gain_i_px;
+          gain[3][1] = msg.Gain_i_py;
+          gain[3][2] = msg.Gain_i_pz;
+          gain[3][3] = msg.Gain_px;
+          gain[3][4] = msg.Gain_py;
+          gain[3][5] = msg.Gain_pz;
+          gain[3][6] = msg.Gain_vx;
+          gain[3][7] = msg.Gain_vy;
+          gain[3][8] = msg.Gain_vz;
+          gain[3][9] = msg.Gain_ax;
+          gain[3][10] = msg.Gain_ay;
+          gain[3][11] = msg.Gain_az;
+          gain[3][12] = msg.Gain_i_roll;
+          gain[3][13] = msg.Gain_i_pitch;
+          gain[3][14] = msg.Gain_i_yaw;
+          gain[3][15] = msg.Gain_roll;
+          gain[3][16] = msg.Gain_pitch;
+          gain[3][17] = msg.Gain_yaw;
+          gain[3][18] = msg.Gain_a_roll;
+          gain[3][19] = msg.Gain_a_pitch;
+          gain[3][20] = msg.Gain_a_yaw;
+        }
+        if(Thruster_selection == 4){
+          offset[4] = msg.offset;
+          gain[4][0] = msg.Gain_i_px;
+          gain[4][1] = msg.Gain_i_py;
+          gain[4][2] = msg.Gain_i_pz;
+          gain[4][3] = msg.Gain_px;
+          gain[4][4] = msg.Gain_py;
+          gain[4][5] = msg.Gain_pz;
+          gain[4][6] = msg.Gain_vx;
+          gain[4][7] = msg.Gain_vy;
+          gain[4][8] = msg.Gain_vz;
+          gain[4][9] = msg.Gain_ax;
+          gain[4][10] = msg.Gain_ay;
+          gain[4][11] = msg.Gain_az;
+          gain[4][12] = msg.Gain_i_roll;
+          gain[4][13] = msg.Gain_i_pitch;
+          gain[4][14] = msg.Gain_i_yaw;
+          gain[4][15] = msg.Gain_roll;
+          gain[4][16] = msg.Gain_pitch;
+          gain[4][17] = msg.Gain_yaw;
+          gain[4][18] = msg.Gain_a_roll;
+          gain[4][19] = msg.Gain_a_pitch;
+          gain[4][20] = msg.Gain_a_yaw;
+        }
+        if(Thruster_selection == 5){
+          offset[5] = msg.offset;
+          gain[5][0] = msg.Gain_i_px;
+          gain[5][1] = msg.Gain_i_py;
+          gain[5][2] = msg.Gain_i_pz;
+          gain[5][3] = msg.Gain_px;
+          gain[5][4] = msg.Gain_py;
+          gain[5][5] = msg.Gain_pz;
+          gain[5][6] = msg.Gain_vx;
+          gain[5][7] = msg.Gain_vy;
+          gain[5][8] = msg.Gain_vz;
+          gain[5][9] = msg.Gain_ax;
+          gain[5][10] = msg.Gain_ay;
+          gain[5][11] = msg.Gain_az;
+          gain[5][12] = msg.Gain_i_roll;
+          gain[5][13] = msg.Gain_i_pitch;
+          gain[5][14] = msg.Gain_i_yaw;
+          gain[5][15] = msg.Gain_roll;
+          gain[5][16] = msg.Gain_pitch;
+          gain[5][17] = msg.Gain_yaw;
+          gain[5][18] = msg.Gain_a_roll;
+          gain[5][19] = msg.Gain_a_pitch;
+          gain[5][20] = msg.Gain_a_yaw;
+        }
+        std::string str = "/home/yash/catkin_ws/src/kraken_3.0/control_system_stack/control_server/parameters/";
+        std::fstream fp;
+        str = str.append(curr_file).c_str();
+        fp.open(str.append(".cp").c_str(), std::ios::trunc | std::ios::out);
+        if(fp.is_open()){
+          _control_parameters[n_map]->write(&fp);
+        }
+        else ROS_INFO("Unable to open file %s", curr_file.c_str());
+    }
 }
-

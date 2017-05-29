3c8f8d980fc6a7d8a7cba434c1ee64a92b131fba
Yashagarwal9 <agarwal.yash94@outlook.com>
2017-05-29 19:56:08 +0530

    Added Server definition for dynamic configuration
    Signed-off-by: Yashagarwal9 <agarwal.yash94@outlook.com>

--- control_system_stack/control_server/src/main.cpp
+++ control_system_stack/control_server/src/main.cpp
@@ -2,10 +2,18 @@
 #include <control_server/ControlServer.h>
 #include "resources/topicHeader.h"
 #include "control_server/loadParam.h"
+#include <dynamic_reconfigure/server.h>
+#include <control_server/paramsConfig.h>
 int main(int argc,char** argv)
 {
     ros::init(argc,argv,"Control_Server");
     ros::NodeHandle n;
+    ros::NodeHandle n_a("~/left_surge");
+    ros::NodeHandle n_b("~/right_surge");
+    ros::NodeHandle n_c("~/front_sway");
+    ros::NodeHandle n_d("~/back_sway");
+    ros::NodeHandle n_e("~/front_heave");
+    ros::NodeHandle n_f("~/back_heave");
 
     if(argc>2)
     {
@@ -25,6 +33,18 @@ int main(int argc,char** argv)
         ros::ServiceServer service1 = n.advertiseService(topics::CONTROL_MOVEALONG_SERV,&kraken_controller::ControlServer::moveAlongLine,&_server);
         ros::ServiceServer service2 = n.advertiseService(topics::CONTROL_SWITCH_CONTROL,&kraken_controller::ControlServer::changeController,&_server);
         ros::ServiceServer service3 = n.advertiseService(topics::CONTROL_LOADPARAM,&kraken_controller::ControlServer::loadParamsCB,&_server);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster0(n_a);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster1(n_b);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster2(n_c);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster3(n_d);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster4(n_e);
+        dynamic_reconfigure::Server<control_server::paramsConfig> thruster5(n_f);
+        thruster0.setCallback(boost::bind(&kraken_controller::ControlServer::callback0, &_server, _1, _2));
+        thruster1.setCallback(boost::bind(&kraken_controller::ControlServer::callback1, &_server, _1, _2));
+        thruster2.setCallback(boost::bind(&kraken_controller::ControlServer::callback2, &_server, _1, _2));
+        thruster3.setCallback(boost::bind(&kraken_controller::ControlServer::callback3, &_server, _1, _2));
+        thruster4.setCallback(boost::bind(&kraken_controller::ControlServer::callback4, &_server, _1, _2));
+        thruster5.setCallback(boost::bind(&kraken_controller::ControlServer::callback5, &_server, _1, _2));
         _server.setServers(&_ser1,&_ser2);
         ros::spin();
     }
